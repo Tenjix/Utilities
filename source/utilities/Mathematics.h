8dd001b8b2ff4b5af088171f7cb7dd64ed804ada
Tenjix <tenjix@gmail.com>
2016-10-21 10:44:27 +0200

    added clamp function and some missing mathematical constants

--- source/utilities/Mathematics.h
+++ source/utilities/Mathematics.h
@@ -4,34 +4,62 @@
 
 // define important constants
 namespace double_constants {
+	// circle circumference to radius ratio
 	constexpr double Tau = 6.28318530717958647693;
 	constexpr double Tau_Inverse = 1.0 / Tau;
 	constexpr double Tau_Half = Tau / 2.0;
+	constexpr double Tau_Half_Inverse = 1.0 / Tau_Half;
+	constexpr double Tau_Quarter = Tau / 4.0;
+	constexpr double Tau_Quarter_Inverse = 1.0 / Tau_Quarter;
+	constexpr double Two_Pi = Tau;
+	constexpr double Two_Pi_Inverse = Tau_Inverse;
+	// circle circumference to diameter ratio
 	constexpr double Pi = Tau_Half;
-	constexpr double Pi_Inverse = 1.0 / Pi;
-	constexpr double Pi_Half = Pi / 2.0;
+	constexpr double Pi_Inverse = Tau_Half_Inverse;
+	constexpr double Pi_Half = Tau_Quarter;
+	constexpr double Pi_Half_Inverse = Tau_Quarter_Inverse;
 	constexpr double Sqrt_2 = 1.41421356237309504880;
 	constexpr double Sqrt_2_Inverse = 1.0 / Sqrt_2;
 	constexpr double Sqrt_3 = 1.73205080756887729353;
 	constexpr double Sqrt_3_Inverse = 1.0 / Sqrt_3;
+	constexpr double Sqrt_5 = 2.23606797749978969641f;
+	constexpr double Sqrt_5_Inverse = 1.0 / Sqrt_5;
 	constexpr double One_Third = 1.0 / 3.0;
 	constexpr double Two_Thirds = 2.0 / 3.0;
+	// euler number
+	constexpr double E = 2.71828182845904523536;
+	// golden ratio
+	constexpr double G = (1.0 + Sqrt_5) / 2.0;
 }
 namespace constd = double_constants;
 
 namespace float_constants {
+	// circle circumference to radius ratio
 	constexpr float Tau = 6.28318530717958647693f;
 	constexpr float Tau_Inverse = 1.0f / Tau;
 	constexpr float Tau_Half = Tau / 2.0f;
+	constexpr float Tau_Half_Inverse = 1.0f / Tau_Half;
+	constexpr float Tau_Quarter = Tau / 4.0f;
+	constexpr float Tau_Quarter_Inverse = 1.0f / Tau_Quarter;
+	constexpr float Two_Pi = Tau;
+	constexpr float Two_Pi_Inverse = Tau_Inverse;
+	// circle circumference to diameter ratio
 	constexpr float Pi = Tau_Half;
-	constexpr float Pi_Inverse = 1.0f / Pi;
-	constexpr float Pi_Half = Pi / 2.0f;
+	constexpr float Pi_Inverse = Tau_Half_Inverse;
+	constexpr float Pi_Half = Tau_Quarter;
+	constexpr float Pi_Half_Inverse = Tau_Quarter_Inverse;
 	constexpr float Sqrt_2 = 1.41421356237309504880f;
 	constexpr float Sqrt_2_Inverse = 1.0f / Sqrt_2;
 	constexpr float Sqrt_3 = 1.73205080756887729353f;
 	constexpr float Sqrt_3_Inverse = 1.0f / Sqrt_3;
+	constexpr float Sqrt_5 = 2.23606797749978969641f;
+	constexpr float Sqrt_5_Inverse = 1.0f / Sqrt_5;
 	constexpr float One_Third = 1.0f / 3.0f;
 	constexpr float Two_Thirds = 2.0f / 3.0f;
+	// euler number
+	constexpr float E = 2.71828182845904523536f;
+	// golden ratio
+	constexpr float G = (1.0f + Sqrt_5) / 2.0f;
 }
 namespace constf = float_constants;
 
@@ -104,4 +132,10 @@ inline float floor(float value, int n) {
 // rounds "value" up to the nearest multiple of 1/"n"
 inline float ceil(float value, int n) {
 	return ceil(value * n) / n;
+}
+
+// clamps "value" into the range [minimum, maximum]
+template <typename Type>
+inline Type clamp(Type value, Type minimum = 0, Type maximum = 1) {
+	return value < minimum ? minimum : (value > maximum ? maximum : value);
 }
